a166ed6a485acb4a9ecb7efb4e78fa0447857757
Casadina <casadina@tuta.io>
2021-11-28 21:56:31 +0800

    Game ends after round 8. Correct answers taken out of list each round. User is presented with score and has the ability to play again at the end.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -15,54 +15,99 @@ struct ContentView: View {
                                     "Ireland", "Italy", "Nigeria",
                                     "Poland", "Russia", "Spain",
                                     "UK", "US"].shuffled()
-    @ State private var correctAnswer = Int.random(in: 0...2)
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    @State private var round = 1
+    @State private var gameOver = false
     
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]),
-                           startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                        .font(.subheadline.weight(.heavy))
-                    
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .shadow(radius: 5)
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                Text ("Round: \(round)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number: number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                Spacer()
+            }
+            .padding()
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your score is \(score)")
+            }
+            .alert("Game Over", isPresented: $gameOver) {
+                Button("Play Again", action: reset)
+            } message: {
+                Text("Great job, your score was \(score)/8")
             }
         }
-        .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
-        } message: {
-            Text("Your score is ???")
-        }
     }
-    func flagTapped(_ number: Int) {
+    
+    func flagTapped(number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
+            round += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Sorry, that is incorrect!\nThe flag you chose was \(countries[number])"
+            round += 1
         }
         
         showingScore = true
+        gameOver = endOfGame()
     }
     
     func askQuestion() {
+        countries.remove(at: correctAnswer)
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func endOfGame() -> Bool {
+        return round > 8
+    }
+    
+    func reset() {
+        score = 0
+        round = 1
     }
 }
     
